8cdc1d4835f76278f74d78e186fb6703b383e179
Michael <mikeb@360replays.com>
2024-04-02 20:02:14 -0700

    renamed json data files to not use an underscore in the name, as GitHub pages (CDN) won't allow downloading of them.

--- src/constants.js
+++ src/constants.js
@@ -1,7 +1,9 @@
 export const settings = {
+  // hosted version of geodata available on GitHub pages.
+  // to host them yourself, download data/data.zip from the repository and extract them to your own server.
   src: 'https://replaysmike.github.io/react-country-state-dropdown/data/',
-  languagesFile: '_languages.json',
-  countriesFile: '_countries.json',
+  languagesFile: 'languages.json',
+  countriesFile: 'countries.json',
   statesFile: 'states-{country}.json',
   citiesFile: 'cities-{country}-{state}.json',
 };
